f3def8eb25dded36aaeeb38d9b8edc491d8dbe1e
lsmcba <lsmcba@gmail.com>
2024-06-27 22:54:36 -0400

    Subiendo mejoras k8s

--- Ejercicio-Kubernetes/ejecutar.sh
+++ Ejercicio-Kubernetes/ejecutar.sh
@@ -3,3 +3,7 @@
 kubectl apply -f namespace.yaml
 kubectl apply -f backend-deploy.yaml
 kubectl apply -f frontend-deploy.yaml
+#kubectl apply -f grafana-deploy.yaml
+#kubectl apply -f prometheus-deploy.yaml
+#kubectl apply -f pyg-config.yaml
+#kubectl apply -f pyg-pvc.yaml
