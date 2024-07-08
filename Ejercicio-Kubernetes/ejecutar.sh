965300d045a4a057eda5d806d6d2cc1bd2740eaa
= <=>
2024-07-08 00:24:24 -0300

    Eliminando grafana de TF

--- Ejercicio-Kubernetes/ejecutar.sh
+++ Ejercicio-Kubernetes/ejecutar.sh
@@ -5,7 +5,7 @@ kubectl apply -f backend-deploy.yaml
 kubectl apply -f frontend-deploy.yaml
 kubectl apply -f grafana-deploy.yaml
 kubectl apply -f prometheus-deploy.yaml
-kubectl apply -f kube-metric-deploy2.yaml
+kubectl apply -f kube-metric-deploy.yaml
 kubectl apply -f cluster-role.yaml
 kubectl apply -f cluster-role-binding.yaml
 kubectl apply -f service-account.yaml
